5eea3fd5b50a3f9e7f917ad74ac65d74d87c72c8
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-03-22 14:59:30 +0100

    added download attempts

--- ground-based-data/pull-input4MIPS.py
+++ ground-based-data/pull-input4MIPS.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+
 # Commands for PS
 # $ uv run esgpull convert ground-based-data/cimp6.txt --graph
 # $ uv run esgpull convert ground-based-data/cimp6.txt --out ground-based-data/cimp6_converted.yaml
@@ -5,14 +20,20 @@
 # $ uv run esgpull show
 # $ uv run esgpull update <id>
 
-
+# %%
 CMIP6_VERSION_PROJECT = "input4MIPs"
-CMIP6_VERSION_MIP_ERA = "CMIP6"
+CMIP6_VERSION_MIP_ERA = "CMIP6Plus"
 CMIP6_VERSION_SOURCE_ID = "UoM-CMIP-1-2-0"
-CMIP6_VERSION_GRID = "gr1-GMNHSH"
+#CMIP6_VERSION_GRID = "gr1-GMNHSH"
 CMIP6_VERSION_FREQUENCY = "yr"
+CIMP6_VARIABLE_ID = "co2"
 SEARCH_TAG = f"cmip6-global-mean-yearly-{CMIP6_VERSION_SOURCE_ID.lower()}"
 
-!uv run esgpull add --tag {SEARCH_TAG} --track project:{CMIP6_VERSION_PROJECT} mip_era:{CMIP6_VERSION_MIP_ERA} source_id:{CMIP6_VERSION_SOURCE_ID} grid_label:{CMIP6_VERSION_GRID} frequency:{CMIP6_VERSION_FREQUENCY}
+# %% [markdown]
+# add data
 
+# %%
+!uv run esgpull add --tag {SEARCH_TAG} --track project:{CMIP6_VERSION_PROJECT} mip_era:{CMIP6_VERSION_MIP_ERA} source_id:{CMIP6_VERSION_SOURCE_ID} frequency:{CMIP6_VERSION_FREQUENCY} variable_id:{CIMP6_VARIABLE_ID}
 
+# %%
+!esgpull update -y --tag {SEARCH_TAG} -X utf8
